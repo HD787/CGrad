8fbbbaa06f6b13051f50ac9d1bbc6f8f4dfc1e5b
henry <Hd116527@gmail.com>
2024-11-14 11:15:32 -0600

    max pool layer generation should be done, untested tho which means it doesnt work

--- layer.c
+++ layer.c
@@ -86,7 +86,7 @@ layer* maxPool2d(tensor* t, int* kernelShape, int* padding, int padDimCount, int
     memcpy(lay->kernelShape, kernelShape, sizeof(int)*2);
     lay->kernelDimCount = 2;
     //the idea is like a conv but the kernel should break it up into even chunks
-    int shape[4] = {t->shape[0], t->shape[1], (t->shape[2] + padding[1])/kernelShape[0], (t->shape[3] + padding[1])};
+    int shape[4] = {t->shape[0], t->shape[1], (t->shape[2] + padding[1])/kernelShape[0], (t->shape[3] + padding[1])/kernelShape[1]};
     lay->activation = createTensor(shape, 4); 
     memcpy(lay->padding, padding, padDimCount);
     lay->weight = NULL;
